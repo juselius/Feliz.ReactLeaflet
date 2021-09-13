6e8a084e35ac1b9ac5f0cbd32cbd3c92bcea7d59
Jonas Juselius <jonas.juselius@itpartner.no>
2021-09-13 17:39:18 +0200

    Unbox event objects

--- src/Events.fs
+++ src/Events.fs
@@ -6,69 +6,69 @@ open Fable.Core.JsInterop
 module BaseEvents =
     [<Erase>]
     type LayerEvents =
-       static member inline add (handler: (Leaflet.LayerEvent -> unit)) = createObj [ "layeradd" ==> handler ]
-       static member inline remove (handler: (Leaflet.LayerEvent -> unit)) = createObj [ "layerremove" ==> handler ]
-       static member inline popupopen (handler: (Leaflet.PopupEvent -> unit)) = createObj [ "popupopen" ==> handler ]
-       static member inline popupclose (handler: (Leaflet.PopupEvent -> unit)) = createObj [ "popupclose" ==> handler ]
-       static member inline tooltipopen (handler: (Leaflet.TooltipEvent -> unit)) = createObj [ "tooltipopen" ==> handler ]
-       static member inline tooltipclose (handler: (Leaflet.TooltipEvent -> unit)) = createObj [ "tooltipclose" ==> handler ]
+       static member inline add (handler: (Leaflet.LayerEvent -> unit)) = createObj [ "layeradd" ==> handler ] |> unbox
+       static member inline remove (handler: (Leaflet.LayerEvent -> unit)) = createObj [ "layerremove" ==> handler ] |> unbox
+       static member inline popupopen (handler: (Leaflet.PopupEvent -> unit)) = createObj [ "popupopen" ==> handler ] |> unbox
+       static member inline popupclose (handler: (Leaflet.PopupEvent -> unit)) = createObj [ "popupclose" ==> handler ] |> unbox
+       static member inline tooltipopen (handler: (Leaflet.TooltipEvent -> unit)) = createObj [ "tooltipopen" ==> handler ] |> unbox
+       static member inline tooltipclose (handler: (Leaflet.TooltipEvent -> unit)) = createObj [ "tooltipclose" ==> handler ] |> unbox
 
     [<Erase>]
     type InteractiveLayerEvents =
        inherit LayerEvents
-       static member inline click (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "click" ==> handler ]
-       static member inline dblclick (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "dblclick" ==> handler ]
-       static member inline mousedown (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "mousedown" ==> handler ]
-       static member inline mouseup (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "mouseup" ==> handler ]
-       static member inline mouseover (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "mouseover" ==> handler ]
-       static member inline mouseout (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "mouseout" ==> handler ]
-       static member inline contextmenu (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "contextmenu" ==> handler ]
+       static member inline click (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "click" ==> handler ] |> unbox
+       static member inline dblclick (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "dblclick" ==> handler ] |> unbox
+       static member inline mousedown (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "mousedown" ==> handler ] |> unbox
+       static member inline mouseup (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "mouseup" ==> handler ] |> unbox
+       static member inline mouseover (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "mouseover" ==> handler ] |> unbox
+       static member inline mouseout (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "mouseout" ==> handler ] |> unbox
+       static member inline contextmenu (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "contextmenu" ==> handler ] |> unbox
 
     [<Erase>]
     type ImageOverlayEvents =
-       static member inline load (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "load" ==> handler ]
-       static member inline error (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "error" ==> handler ]
+       static member inline load (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "load" ==> handler ] |> unbox
+       static member inline error (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "error" ==> handler ] |> unbox
 
 [<Erase>]
 type mapEvent =
     inherit BaseEvents.InteractiveLayerEvents
-    static member inline layeradd (handler: (Leaflet.LayerEvent -> unit)) = createObj [ "layeradd" ==> handler ]
-    static member inline layerremove (handler: (Leaflet.LayerEvent -> unit)) = createObj [ "layerremove" ==> handler ]
-    static member inline baselayerchange (handler: (Leaflet.LayersControlEvent -> unit)) = createObj [ "baselayerchange" ==> handler ]
-    static member inline overlayeradd (handler: (Leaflet.LayersControlEvent -> unit)) = createObj [ "overlayeradd" ==> handler ]
-    static member inline overlayerremove (handler: (Leaflet.LayersControlEvent -> unit)) = createObj [ "overlayerremove" ==> handler ]
-    static member inline zoom (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "zoom" ==> handler ]
-    static member inline zoomstart (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "zoomstart" ==> handler ]
-    static member inline zoomend (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "zoomend" ==> handler ]
-    static member inline zoomlevelschange (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "zoomlevelschange" ==> handler ]
-    static member inline resize (handler: (Leaflet.ResizeEvent -> unit)) = createObj [ "resize" ==> handler ]
-    static member inline load (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "load" ==> handler ]
-    static member inline unload (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "unload" ==> handler ]
-    static member inline viewreset (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "viewreset" ==> handler ]
-    static member inline move (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "move" ==> handler ]
-    static member inline movestart (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "movestart" ==> handler ]
-    static member inline moveend (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "moveend" ==> handler ]
-    static member inline popupopen (handler: (Leaflet.PopupEvent -> unit)) = createObj [ "popupopen" ==> handler ]
-    static member inline popupclose (handler: (Leaflet.PopupEvent -> unit)) = createObj [ "popupclose" ==> handler ]
-    static member inline autopanstart (handler: (Leaflet.PopupEvent -> unit)) = createObj [ "autopanstart" ==> handler ]
-    static member inline tooltipopen (handler: (Leaflet.TooltipEvent -> unit)) = createObj [ "tooltipopen" ==> handler ]
-    static member inline tooltipclose (handler: (Leaflet.TooltipEvent -> unit)) = createObj [ "tooltipclose" ==> handler ]
-    static member inline locationfound (handler: (Leaflet.LocationEvent -> unit)) = createObj [ "locationfound" ==> handler ]
-    static member inline locationerror (handler: (Leaflet.ErrorEvent -> unit)) = createObj [ "locationerror" ==> handler ]
-    static member inline mousemove (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "mousemove" ==> handler ]
-    static member inline keypress (handler: (Leaflet.LeafletKeyboardEvent -> unit)) = createObj [ "keypress" ==> handler ]
-    static member inline keydown (handler: (Leaflet.LeafletKeyboardEvent -> unit)) = createObj [ "keydown" ==> handler ]
-    static member inline keyup (handler: (Leaflet.LeafletKeyboardEvent -> unit)) = createObj [ "keyup" ==> handler ]
-    static member inline preclick (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "preclick" ==> handler ]
+    static member inline layeradd (handler: (Leaflet.LayerEvent -> unit)) = createObj [ "layeradd" ==> handler ] |> unbox
+    static member inline layerremove (handler: (Leaflet.LayerEvent -> unit)) = createObj [ "layerremove" ==> handler ] |> unbox
+    static member inline baselayerchange (handler: (Leaflet.LayersControlEvent -> unit)) = createObj [ "baselayerchange" ==> handler ] |> unbox
+    static member inline overlayeradd (handler: (Leaflet.LayersControlEvent -> unit)) = createObj [ "overlayeradd" ==> handler ] |> unbox
+    static member inline overlayerremove (handler: (Leaflet.LayersControlEvent -> unit)) = createObj [ "overlayerremove" ==> handler ] |> unbox
+    static member inline zoom (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "zoom" ==> handler ] |> unbox
+    static member inline zoomstart (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "zoomstart" ==> handler ] |> unbox
+    static member inline zoomend (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "zoomend" ==> handler ] |> unbox
+    static member inline zoomlevelschange (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "zoomlevelschange" ==> handler ] |> unbox
+    static member inline resize (handler: (Leaflet.ResizeEvent -> unit)) = createObj [ "resize" ==> handler ] |> unbox
+    static member inline load (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "load" ==> handler ] |> unbox
+    static member inline unload (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "unload" ==> handler ] |> unbox
+    static member inline viewreset (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "viewreset" ==> handler ] |> unbox
+    static member inline move (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "move" ==> handler ] |> unbox
+    static member inline movestart (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "movestart" ==> handler ] |> unbox
+    static member inline moveend (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "moveend" ==> handler ] |> unbox
+    static member inline popupopen (handler: (Leaflet.PopupEvent -> unit)) = createObj [ "popupopen" ==> handler ] |> unbox
+    static member inline popupclose (handler: (Leaflet.PopupEvent -> unit)) = createObj [ "popupclose" ==> handler ] |> unbox
+    static member inline autopanstart (handler: (Leaflet.PopupEvent -> unit)) = createObj [ "autopanstart" ==> handler ] |> unbox
+    static member inline tooltipopen (handler: (Leaflet.TooltipEvent -> unit)) = createObj [ "tooltipopen" ==> handler ] |> unbox
+    static member inline tooltipclose (handler: (Leaflet.TooltipEvent -> unit)) = createObj [ "tooltipclose" ==> handler ] |> unbox
+    static member inline locationfound (handler: (Leaflet.LocationEvent -> unit)) = createObj [ "locationfound" ==> handler ] |> unbox
+    static member inline locationerror (handler: (Leaflet.ErrorEvent -> unit)) = createObj [ "locationerror" ==> handler ] |> unbox
+    static member inline mousemove (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "mousemove" ==> handler ] |> unbox
+    static member inline keypress (handler: (Leaflet.LeafletKeyboardEvent -> unit)) = createObj [ "keypress" ==> handler ] |> unbox
+    static member inline keydown (handler: (Leaflet.LeafletKeyboardEvent -> unit)) = createObj [ "keydown" ==> handler ] |> unbox
+    static member inline keyup (handler: (Leaflet.LeafletKeyboardEvent -> unit)) = createObj [ "keyup" ==> handler ] |> unbox
+    static member inline preclick (handler: (Leaflet.LeafletMouseEvent -> unit)) = createObj [ "preclick" ==> handler ] |> unbox
 
 type markerEvent =
     inherit BaseEvents.InteractiveLayerEvents
-    static member inline move (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "move" ==> handler ]
-    static member inline dragstart (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "dragstart" ==> handler ]
-    static member inline movestart (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "movestart" ==> handler ]
-    static member inline drag (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "drag" ==> handler ]
-    static member inline dragend (handler: (Leaflet.DragEndEvent -> unit)) = createObj [ "dragend" ==> handler ]
-    static member inline moveend (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "moveend" ==> handler ]
+    static member inline move (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "move" ==> handler ] |> unbox
+    static member inline dragstart (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "dragstart" ==> handler ] |> unbox
+    static member inline movestart (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "movestart" ==> handler ] |> unbox
+    static member inline drag (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "drag" ==> handler ] |> unbox
+    static member inline dragend (handler: (Leaflet.DragEndEvent -> unit)) = createObj [ "dragend" ==> handler ] |> unbox
+    static member inline moveend (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "moveend" ==> handler ] |> unbox
 
 [<Erase>]
 type layerEvent = BaseEvents.LayerEvents
@@ -76,29 +76,38 @@ type layerEvent = BaseEvents.LayerEvents
 [<Erase>]
 type gridLayerEvent =
    inherit BaseEvents.LayerEvents
-   static member inline loading (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "loading" ==> handler ]
-   static member inline tileloadstart (handler: (Leaflet.TileEvent -> unit)) = createObj [ "tileloadstart" ==> handler ]
-   static member inline tileload (handler: (Leaflet.TileEvent -> unit)) = createObj [ "tileload" ==> handler ]
-   static member inline tileunload (handler: (Leaflet.TileEvent -> unit)) = createObj [ "tileunload" ==> handler ]
-   static member inline tileerror (handler: (Leaflet.TileEvent -> unit)) = createObj [ "tileerror" ==> handler ]
-   static member inline load (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "load" ==> handler ]
+   static member inline loading (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "loading" ==> handler ] |> unbox
+   static member inline tileloadstart (handler: (Leaflet.TileEvent -> unit)) = createObj [ "tileloadstart" ==> handler ] |> unbox
+   static member inline tileload (handler: (Leaflet.TileEvent -> unit)) = createObj [ "tileload" ==> handler ] |> unbox
+   static member inline tileunload (handler: (Leaflet.TileEvent -> unit)) = createObj [ "tileunload" ==> handler ] |> unbox
+   static member inline tileerror (handler: (Leaflet.TileEvent -> unit)) = createObj [ "tileerror" ==> handler ] |> unbox
+   static member inline load (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "load" ==> handler ] |> unbox
 
 [<Erase>]
-type pathEvent = BaseEvents.InteractiveLayerEvents
+type circleEvent = BaseEvents.InteractiveLayerEvents
+
+[<Erase>]
+type polygonEvent = BaseEvents.InteractiveLayerEvents
+
+[<Erase>]
+type rectangleEvent = BaseEvents.InteractiveLayerEvents
+
+[<Erase>]
+type polylineEvent = BaseEvents.InteractiveLayerEvents
 
 [<Erase>]
 type circleMarkerEvent =
     inherit BaseEvents.InteractiveLayerEvents
-    static member inline move (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "move" ==> handler ]
+    static member inline move (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "move" ==> handler ] |> unbox
 
 [<Erase>]
 type geoJSONEvent =
     inherit BaseEvents.LayerEvents
-    static member inline layeradd (handler: (Leaflet.LayerEvent -> unit)) = createObj [ "layeradd" ==> handler ]
-    static member inline layerremove (handler: (Leaflet.LayerEvent -> unit)) = createObj [ "layerremove" ==> handler ]
+    static member inline layeradd (handler: (Leaflet.LayerEvent -> unit)) = createObj [ "layeradd" ==> handler ] |> unbox
+    static member inline layerremove (handler: (Leaflet.LayerEvent -> unit)) = createObj [ "layerremove" ==> handler ] |> unbox
 
 [<Erase>]
 type imageOverlayEvent =
     inherit BaseEvents.InteractiveLayerEvents
-    static member inline load (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "load" ==> handler ]
-    static member inline error (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "error" ==> handler ]
+    static member inline load (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "load" ==> handler ] |> unbox
+    static member inline error (handler: (Leaflet.LeafletEvent -> unit)) = createObj [ "error" ==> handler ] |> unbox
--- src/VectorLayer.fs
+++ src/VectorLayer.fs
@@ -9,7 +9,7 @@ type circle =
     static member inline center (value: Leaflet.LatLngExpression) = Interop.mkCircleProp "center" value
     static member inline pathOptions (value: IPathOption list) = unbox<ICircleProp> ("pathOptions", createObj !!value)
     static member inline radius (value: float) = Interop.mkCircleProp "radius" value
-    static member inline eventHandlers (value: pathEvent list) = unbox<'a> ("eventHandlers", keyValueList CaseRules.LowerFirst value)
+    static member inline eventHandlers (value: circleEvent list) = unbox<ICircleProp> ("eventHandlers", keyValueList CaseRules.LowerFirst value)
 
 [<Erase>]
 type circleMarker =
@@ -28,7 +28,7 @@ type polyline =
     static member inline positions (value: Leaflet.LatLngExpression [][][]) = Interop.mkPolylineProp "positions" value
     static member inline smoothFactor (value: float) = Interop.mkPolylineProp "smoothFactor" value
     static member inline noClip (value: bool) = Interop.mkPolylineProp "noClip" value
-    static member inline eventHandlers (value: pathEvent list) = unbox<'a> ("eventHandlers", keyValueList CaseRules.LowerFirst value)
+    static member inline eventHandlers (value: polylineEvent list) = unbox<'a> ("eventHandlers", keyValueList CaseRules.LowerFirst value)
 
 [<Erase>]
 type polygon =
@@ -39,7 +39,7 @@ type polygon =
     static member inline positions (value: Leaflet.LatLngExpression [][][]) = Interop.mkPolygonProp "positions" value
     static member inline smoothFactor (value: float) = Interop.mkPolygonProp "smoothFactor" value
     static member inline noClip (value: bool) = Interop.mkPolygonProp "noClip" value
-    static member inline eventHandlers (value: pathEvent list) = unbox<'a> ("eventHandlers", keyValueList CaseRules.LowerFirst value)
+    static member inline eventHandlers (value: polygonEvent list) = unbox<'a> ("eventHandlers", keyValueList CaseRules.LowerFirst value)
 
 [<Erase>]
 type rectangle =
@@ -47,7 +47,7 @@ type rectangle =
     static member inline bounds (value: Leaflet.LatLngBoundsExpression) = Interop.mkRectangleProp "bounds" value
     static member inline pathOptions (value: Leaflet.PathOptions) = Interop.mkRectangleProp "pathOptions" value
     static member inline popupContainer (value: Leaflet.FeatureGroup<obj>) = Interop.mkRectangleProp "popupContainer" value
-    static member inline eventHandlers (value: pathEvent list) = unbox<'a> ("eventHandlers", keyValueList CaseRules.LowerFirst value)
+    static member inline eventHandlers (value: rectangleEvent list) = unbox<'a> ("eventHandlers", keyValueList CaseRules.LowerFirst value)
 
 // [<Erase>]
 // type svgOverlay =
