9f63f9a497e8ead88f5a26ef18860475b8706ce4
Jonas Juselius <jonas.juselius@tromso.serit.no>
2021-10-20 10:29:59 +0200

    fix: builder

--- .build/Build.fs
+++ .build/Build.fs
@@ -7,21 +7,30 @@ open Helpers
 
 initializeContext()
 
-let srcPath    = Path.getFullName "src"
-let testPath  = Path.getFullName "test"
+let clientPath = Path.getFullName "src/Client"
+let sharedPath = Path.getFullName "src/Shared"
+let serverPath = Path.getFullName "src/Server"
+let testPath   = Path.getFullName "test"
+let libPath    = Path.getFullName "src"
+
 let deployPath = Path.getFullName "deploy"
 let packPath = Path.getFullName "packages"
+let versionFile = Path.getFullName ".version"
 
 Target.create "Clean" (fun _ -> Shell.cleanDir deployPath)
 
 Target.create "InstallClient" (fun _ -> run npm "install" ".")
 
 Target.create "Bundle" (fun _ ->
-    run dotnet $"publish -c Release -o \"{deployPath}\"" srcPath
+    run dotnet $"publish -c Release -o \"{deployPath}\"" libPath
+)
+
+Target.create "BundleDebug" (fun _ ->
+    run dotnet $"publish -c Debug -o \"{deployPath}\"" libPath
 )
 
 Target.create "Pack" (fun _ ->
-    run dotnet $"pack -c Release -o \"{packPath}\"" srcPath
+    run dotnet $"pack -c Release -o \"{packPath}\"" libPath
 )
 
 Target.create "Run" (fun _ ->
@@ -32,11 +41,24 @@ Target.create "Format" (fun _ ->
     run dotnet "fantomas . -r" "src"
 )
 
+Target.create "Test" (fun _ ->
+    if System.IO.Directory.Exists testPath then
+      run dotnet "run" testPath
+    else ()
+)
+
 open Fake.Core.TargetOperators
 
 let dependencies = [
     "Clean"
+        ==> "InstallClient"
         ==> "Bundle"
+
+    "Clean"
+        ==> "BundleDebug"
+
+    "Clean"
+        ==> "Test"
 
     "Clean"
         ==> "InstallClient"
